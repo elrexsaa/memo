e49d61e06608c44d0ee0e47fea12fe2b57b9fb82
Rexsaa <frankandrewwzz@gmail.com>
2025-12-12 23:31:47 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/main.css';
-import './styles/animations.css';
 import App from './App';
+
+// Import Tailwind CSS
+import './styles/tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
